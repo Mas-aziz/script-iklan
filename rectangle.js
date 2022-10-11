da77137953a31bd4039ac9006fd158b203aa1393
Mas-aziz <115584069+Mas-aziz@users.noreply.github.com>
2022-10-12 01:57:25 +0700

    Create rectangle.js

---
+++ rectangle.js
@@ -0,0 +1,23 @@
+var cat1 = [
+  "https://ads-load-rectangle.blogspot.com/2022/06/iklan1.html",
+  "https://ads-load-rectangle.blogspot.com/2022/06/iklan2.html",
+  "https://ads-load-rectangle.blogspot.com/2022/06/iklan3.html",
+  "https://ads-load-rectangle.blogspot.com/2022/06/iklan4.html",
+  "https://ads-load-rectangle.blogspot.com/2022/06/iklan5.html",
+];
+
+var myFrame = document.getElementsByClassName("frame")[0];
+
+function getRandomUrl(myFrame) {
+  var index = Math.floor(Math.random() * cat1.length);
+  var url = cat1[index];
+  myFrame.src = url;
+}
+
+function codeAddress() {
+  getRandomUrl(myFrame);
+}
+
+codeAddress();
+
+console.log(myFrame.src);
