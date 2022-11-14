200487e9c17278757884248ebb474a5ba2f307c3
Mas-aziz <115584069+Mas-aziz@users.noreply.github.com>
2022-11-15 03:32:59 +0700

    Update ads.js

--- data/pop-up/ads.js
+++ data/pop-up/ads.js
@@ -1,3 +1,9 @@
 document.write( '<script type="text/css" src="https://script-iklan.pages.dev/data/pop-up/style.css"></script>' );
-document.write( '<div class='popbox csspop' id='popbox'><div aria-label='Close' class='pop-overlay' onclick='document.getElementById(&quot;popbox&quot;).style.display=&quot;none&quot;;removeClassonBody();' role='button' tabindex='0'/><div class='pop-content'>Klick to close ads<div class='popcontent'><script src="https://script-iklan.pages.dev/data/rectangle/banner.js"></script></div></div></div></div>' );
+document.write( '<div class="popbox csspop" id="popbox">
+<div aria-label="Close" class="pop-overlay" onclick="document.getElementById(&quot;popbox&quot;).style.display=&quot;none&quot;;removeClassonBody();" role="button" tabindex="0"/>
+<div class="pop-content">
+Klick to close ads
+<div class="popcontent">
+<script src="https://script-iklan.pages.dev/data/rectangle/banner.js"></script>
+</div></div></div></div>' );
 document.write( '<script src="https://script-iklan.pages.dev/data/pop-up/java.js"></script>' );
