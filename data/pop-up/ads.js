b1fb1aca4c89107a50c8da9b3d6dbe7bb64874d9
Mas-aziz <115584069+Mas-aziz@users.noreply.github.com>
2022-11-15 03:20:08 +0700

    Update ads.js

--- data/pop-up/ads.js
+++ data/pop-up/ads.js
@@ -1,17 +1,4 @@
 document.write( '<script type="text/css" src="https://script-iklan.pages.dev/data/pop-up/style.css"></script>' );
-document.write( '<div class='popbox csspop' id='popbox'>
-<div aria-label='Close' class='pop-overlay' onclick='document.getElementById(&quot;popbox&quot;).style.display=&quot;none&quot;;removeClassonBody();' role='button' tabindex='0'/>
-<div class='pop-content'>
-Klick to close ads
-<div class='popcontent'>
-<script src="https://script-iklan.pages.dev/data/rectangle/banner.js"></script>
-</div></div></div></div>' );
-document.write( '<script>
-//<![CDATA[
-setTimeout(function(){
- document.getElementById('popbox').classList.remove('csspop');
- document.body.className+=" flowbox"
- }, 5000);
-function removeClassonBody(){var element=document.body;element.className=element.className.replace(/\bflowbox\b/g,"")}
-//]]>
- </script>' );
+document.write( '<div class='popbox csspop' id='popbox'><div aria-label='Close' class='pop-overlay' onclick='document.getElementById(&quot;popbox&quot;).style.display=&quot;none&quot;;removeClassonBody();' role='button' tabindex='0'/><div class='pop-content'>Klick to close ads<div class='popcontent'>
+<script src="https://script-iklan.pages.dev/data/rectangle/banner.js"></script></div></div></div></div>' );
+document.write( '<script>//<![CDATA[setTimeout(function(){ document.getElementById('popbox').classList.remove('csspop'); document.body.className+=" flowbox" }, 5000);function removeClassonBody(){var element=document.body;element.className=element.className.replace(/\bflowbox\b/g,"")}//]]></script>' );
