df0e080bd2eedc014c8864ab14c0afb21aa9225b
Mas-aziz <115584069+Mas-aziz@users.noreply.github.com>
2022-11-15 08:23:45 +0700

    Create ads-link.js

---
+++ data/rectangle/adsload/ads-link.js
@@ -0,0 +1,8 @@
+"https://ads-load-rectangle.blogspot.com/2022/06/iklan1.html",
+"https://ads-load-rectangle.blogspot.com/2022/06/iklan2.html",
+"https://ads-load-rectangle.blogspot.com/2022/06/iklan3.html",
+"https://ads-load-rectangle.blogspot.com/2022/06/iklan4.html",
+"https://ads-load-rectangle.blogspot.com/2022/06/iklan5.html",
+"https://ads-load-rectangle.blogspot.com/p/pasang-iklan.html",
+"https://ads-load-rectangle.blogspot.com/2022/11/nike-ads.html",
+"https://ads-load-rectangle.blogspot.com/2022/11/rekomendasi.html",
