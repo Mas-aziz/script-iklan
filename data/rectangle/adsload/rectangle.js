6017ca0dc6c366f429203e5d2a7bcff4d5aef448
Mas-aziz <115584069+Mas-aziz@users.noreply.github.com>
2022-11-15 09:21:45 +0700

    Update rectangle.js

--- data/rectangle/adsload/rectangle.js
+++ data/rectangle/adsload/rectangle.js
@@ -1,12 +1,11 @@
-var cat1 = [
-document.write( '<script src="https://script-iklan.pages.dev/data/rectangle/adsload/ads-link.js"></script>' )
-];
+var cat1 = [];
 
 var myFrame = document.getElementsByClassName("frame")[0];
 
 function getRandomUrl(myFrame) {
   var index = Math.floor(Math.random() * cat1.length);
-  var url = cat1[index];
+  var url = cat1[index(document.write( '<script src="https://script-iklan.pages.dev/data/rectangle/adsload/ads-link.js"></script>' )
+)];
   myFrame.src = url;
 }
 
