de084ac2a363c8a23d609c5958a0b754589d5a12
Mas-aziz <115584069+Mas-aziz@users.noreply.github.com>
2022-11-15 10:53:50 +0700

    Update rect-sit.js

--- ads/rect-sit.js
+++ ads/rect-sit.js
@@ -1,9 +1,5 @@
 var cat1 = [
-"<?=$url = "http://ads-load-rectangle.blogspot.com/sitemap.xml" ;
-$xml = simplexml_load_file($url);
-$link = $xml->url[mt_rand(0,count($xml->url)-1)]->loc ; // Get Random Location 
-
-Header("Location:" . $link)?>"
+"document.write( '<script src="https://script-iklan.pages.dev/data/rectangle/adsload/ads-link.js"></script>' )"
 ];
 
 var myFrame = document.getElementsByClassName("frame")[0];
