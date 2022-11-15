36e15d67b6c9321472e56f3adec3fa1389535c09
Mas-aziz <115584069+Mas-aziz@users.noreply.github.com>
2022-11-15 11:43:09 +0700

    Update ads-link.js

--- data/rectangle/adsload/ads-link.js
+++ data/rectangle/adsload/ads-link.js
@@ -1,4 +1,4 @@
-var qwer1 = [
+[
 "https://ads-load-rectangle.blogspot.com/2022/06/iklan1.html",
 "https://ads-load-rectangle.blogspot.com/2022/06/iklan2.html",
 "https://ads-load-rectangle.blogspot.com/2022/06/iklan3.html",
@@ -7,4 +7,4 @@ var qwer1 = [
 "https://ads-load-rectangle.blogspot.com/p/pasang-iklan.html",
 "https://ads-load-rectangle.blogspot.com/2022/11/nike-ads.html",
 "https://ads-load-rectangle.blogspot.com/2022/11/rekomendasi.html",
-];
+]
