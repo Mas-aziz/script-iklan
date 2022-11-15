afb4f687712ee605ec60e88c98775b75d3c10fdd
Mas-aziz <115584069+Mas-aziz@users.noreply.github.com>
2022-11-15 09:26:07 +0700

    Update rectangle.js

--- data/rectangle/adsload/rectangle.js
+++ data/rectangle/adsload/rectangle.js
@@ -1,11 +1,19 @@
-var cat1 = [];
+var cat1 = [
+"https://ads-load-rectangle.blogspot.com/2022/06/iklan1.html",
+"https://ads-load-rectangle.blogspot.com/2022/06/iklan2.html",
+"https://ads-load-rectangle.blogspot.com/2022/06/iklan3.html",
+"https://ads-load-rectangle.blogspot.com/2022/06/iklan4.html",
+"https://ads-load-rectangle.blogspot.com/2022/06/iklan5.html",
+"https://ads-load-rectangle.blogspot.com/p/pasang-iklan.html",
+"https://ads-load-rectangle.blogspot.com/2022/11/nike-ads.html",
+"https://ads-load-rectangle.blogspot.com/2022/11/rekomendasi.html",
+];
 
-var myFrame = document.getElementsByClassName("frame")[0];
+var myFrame = document.getElementsByClassName("frame","rsp-ol")[0];
 
 function getRandomUrl(myFrame) {
   var index = Math.floor(Math.random() * cat1.length);
-  var url = cat1[index(document.write( '<script src="https://script-iklan.pages.dev/data/rectangle/adsload/ads-link.js"></script>' )
-)];
+  var url = cat1[index];
   myFrame.src = url;
 }
 
