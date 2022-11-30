6ab9f0bd39e01765461cc3de6f8109613be61dba
Mas-aziz <115584069+Mas-aziz@users.noreply.github.com>
2022-11-30 09:46:19 +0700

    Update script-ads.js

--- data/displayslider/script-ads.js
+++ data/displayslider/script-ads.js
@@ -1,2 +1,2 @@
-document.write ( '<style src="https://script-iklan.pages.dev/data/displayslider/style.css"></style>' );
+document.write ( '<link href="https://script-iklan.pages.dev/data/displayslider/style.css" rel="stylesheet"/>' );
 document.write ( '<script src="https://script-iklan.pages.dev/data/displayslider/javascript.js"></script>' );
