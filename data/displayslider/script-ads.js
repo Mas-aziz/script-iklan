b0205a788807d75cd8f93cac10ae6b088902e9cd
Mas-aziz <115584069+Mas-aziz@users.noreply.github.com>
2022-11-30 09:25:11 +0700

    Create script-ads.js

---
+++ data/displayslider/script-ads.js
@@ -0,0 +1,2 @@
+document.write ( '<script src="https://script-iklan.pages.dev/data/displayslider/style.css"></script>' );
+document.write ( '<script src="https://script-iklan.pages.dev/data/displayslider/javascript.js"></script>' );
