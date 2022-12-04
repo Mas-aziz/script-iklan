cec5bfd67cd70732f42b4210633cf3193b976e8e
Mas-aziz <115584069+Mas-aziz@users.noreply.github.com>
2022-12-04 12:37:36 +0700

    Update banner.js

--- data/rectangle/banner.js
+++ data/rectangle/banner.js
@@ -1,3 +1,2 @@
 document.write( '<center><iframe class="frame" src="" style="width:300px; height:250px; border:0px; padding:0; overflow:hidden; background-color: transparent" scrolling="no"></iframe></center>' );
 document.write( '<script src="https://script-iklan.pages.dev/data/rectangle/adsload/rectangle.js"></script>' );
-document.write( '<script src="https://script-iklan.pages.dev/ads/directlink/youtube.js"></script>' );
