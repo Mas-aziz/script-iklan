b9523dd7c137443ffba8296b1c13c8fb6633e2dc
Mas-aziz <115584069+Mas-aziz@users.noreply.github.com>
2023-12-11 20:52:50 +0700

    Update java.js

--- data/pop-up/java.js
+++ data/pop-up/java.js
@@ -2,7 +2,7 @@
 setTimeout(function(){
  document.getElementById('popbox').classList.remove('csspop');
  document.body.className+=" flowbox"
- }, 5000);
+ }, 1000);
 function removeClassonBody(){var element=document.body;element.className=element.className.replace(/\bflowbox\b/g,"")}
 //]]>
  
